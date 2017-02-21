56c4df508caafc67b3c092e57a36fd85c7545fc4
jesse snyder <jessetsnyder@gmail.com>
2017-02-20 22:49:15 -0700

    installed react-desktop for native looking ui

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,11 +1,31 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+import { TextInput } from 'react-desktop/macOs';
+
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      val: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ val: e.value });
+  }
+
   render() {
+    const { val } = this.state;
     return (
       <div>
         Home page
+        <TextInput
+          label="Type in this"
+          placeholder="Type in this"
+          value={val}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
